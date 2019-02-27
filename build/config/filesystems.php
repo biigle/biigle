1ab7dc84f087f048f0422014fad84e6ba6dda8ba
Martin Zurowietz <martin@zurowietz.de>
2019-02-27 15:27:03 +0100

    Update for new thumbnail storage disk

--- build/config/filesystems.php
+++ build/config/filesystems.php
@@ -55,6 +55,13 @@ return [
             'root' => storage_path('tiles'),
         ],
 
+        'thumbs' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/thumbs'),
+            'url' => env('APP_URL').'/storage/thumbs',
+            'visibility' => 'public',
+        ],
+
         'largo' => [
             'driver' => 'local',
             'root' => storage_path('app/public/largo-patches'),
